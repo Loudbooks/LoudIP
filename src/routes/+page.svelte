326b1d31ad9ac99048f71a356e77f0775bf29f2e
Loudbook <loudbooked@gmail.com>
2024-09-15 21:11:40 -0400

    Use clipboard icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -117,9 +117,9 @@
             </h1>
         </div>
 
-        <p bind:this={copied}>
-            Copied.
-        </p>
+        <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" bind:this={copied}>
+            <path d="M8 5.00005C7.01165 5.00082 6.49359 5.01338 6.09202 5.21799C5.71569 5.40973 5.40973 5.71569 5.21799 6.09202C5 6.51984 5 7.07989 5 8.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.07989 21 8.2 21H15.8C16.9201 21 17.4802 21 17.908 20.782C18.2843 20.5903 18.5903 20.2843 18.782 19.908C19 19.4802 19 18.9201 19 17.8V8.2C19 7.07989 19 6.51984 18.782 6.09202C18.5903 5.71569 18.2843 5.40973 17.908 5.21799C17.5064 5.01338 16.9884 5.00082 16 5.00005M8 5.00005V7H16V5.00005M8 5.00005V4.70711C8 4.25435 8.17986 3.82014 8.5 3.5C8.82014 3.17986 9.25435 3 9.70711 3H14.2929C14.7456 3 15.1799 3.17986 15.5 3.5C15.8201 3.82014 16 4.25435 16 4.70711V5.00005M12 11H9M15 15H9" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
     </div>
     <div id="background" bind:this={background} use:randomize>
         {#each {length: amountNeededVertical} as _, i}
@@ -209,11 +209,9 @@
         color: rgb(255, 255, 255, 0.05);
         margin-right: 40px;
     }
-    p {
-        color: darkgray;
-        font-family: Gabarito;
-        font-weight: 100;
-        font-size: min(3vw, 20px);
+    svg {
+        height: min(5vw, 50px);
+        width: min(5vw, 50px);
         margin: 0;
         padding: 0;
         position: absolute;
